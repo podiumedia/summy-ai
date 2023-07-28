5ebfa04def2ce3534c5a450aee7ae8bbd86a2a5d
podiumedia <leon@podiumedia.com>
2023-07-28 19:11:10 -0400

    update 2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<HashRouter basename='/summy-ai'>
+<HashRouter>
       <Routes>
       <Route path="/" element={<App />} />
     </Routes>
